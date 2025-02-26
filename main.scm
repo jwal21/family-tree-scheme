df3fc14e176d038a64c2ddf14ebf3972b08785b8
Jude Walden <judewalden3@gmail.com>
2025-02-26 00:00:45 +0000

    A1 updated

--- main.scm
+++ main.scm
@@ -22,25 +22,19 @@
   ((John Bloom) ((Greta Blake) (Ned Bloom)) ((5 12 2023) ())))
 )
 
-;,Paternal branch
 (define Pb
-'(((John Smith) ((Jane Doe) (Fred Smith)) ((1 12 1956) (3 3 2021))) 
-((Ana Smith) ((Jane Doe) (Fred Smith)) ((6 10 1958) ()))
-((Jane Doe) ((Eve Talis) (John Doe)) ((2 6 1930) (4 12 1992)))
-((Fred Smith) ((Lisa Brown) (Tom Smith)) ((17 2 1928) (13 9 2016)))
-((Eve Talis) (() ()) ((15 5 1900) (19 7 1978)))
-((John Doe) (() ()) ((18 2 1899)(7 7 1970)))
-((Lisa Brown) (() ())((31 6 1904) (6 3 1980)))
-((Tom Smith) (() ()) ((2 8 1897) (26 11 1987)))
-((Alan Doe) ((Eve Talis) (John Doe)) ((8 9 1932) (23 12 2000)))
-((Mary Doe) (() (Alan Doe)) ((14 4 1964) ()))))
+  '(((John Smith) ((Jane Doe) (Fred Smith)) ((1 12 1956) (3 3 2021))) 
+    ((Ana Smith) ((Jane Doe) (Fred Smith)) ((6 10 1958) ()))
+    ((Jane Doe) ((Eve Talis) (John Doe)) ((2 6 1930) (4 12 1992)))
+    ((Fred Smith) ((Lisa Brown) (Tom Smith)) ((17 2 1928) (13 9 2016)))
+    ((Eve Talis) (() ()) ((15 5 1900) (19 7 1978)))
+    ((John Doe) (() ()) ((18 2 1899)(7 7 1970)))
+    ((Lisa Brown) (() ())((31 6 1904) (6 3 1980)))
+    ((Tom Smith) (() ()) ((2 8 1897) (26 11 1987)))
+    ((Alan Doe) ((Eve Talis) (John Doe)) ((8 9 1932) (23 12 2000)))
+    ((Mary Doe) (() (Alan Doe)) ((14 4 1964) ())))
+)
 
-;;define lst-mb
-;;define lst-pb
-;;define lst-all
-
-;; C1
-; Print the first and last name of for each person (maternal branch)
 (define (lst-mb mb)
   (if (null? mb)
     ()
@@ -65,15 +59,20 @@
   (append-lst mb pb))
 
 ;; A1 return the parents in the given familial branch
-(define (parents lst)
-  (apply append                          ; Gets rid of the extra nesting
-         (map (lambda (entry)                     ; Applies lambda func to each sublist
-                (let ((parent-pair (cadr entry)))          ; Accesses Parent list in the sublist
-                (filter (lambda (x) (not (null? x))) parent-pair)))      ; Removes empty lists if present in Parent list
-         lst)))
+(define (parents lst)   
+  (define (delete-dup x)   ; Helper function that deletes duplicates in the list
+    (if (null? x) '()
+        (cons (car x) (delete-dup (filter (lambda (y) (not (equal? y (car x))))
+                                          (cdr x))))))
+  (delete-dup   ; Calling helper function
+   (apply append                          
+           (map (lambda (entry)   ; Applies lambda func to each sublist
+                  (let ((parent-pair (cadr entry)))   ; Accesses parents in the sublist
+                    (filter (lambda (x) (not (null? x))) parent-pair)))   ; Removes empty lists if present in parents list
+                lst))))
 
 
-(parents Mb)
+
 ;; A2
 (define (living-members lst)
   ())
@@ -121,13 +120,11 @@
   
 ;;
 ;;You should include code to execute each of your functions below.
-(define ESC #\033)
-(define CSI (list->string (list ESC #\[ )))
-(define CLEAR (string-append CSI "2J"))
-(display CLEAR)
 (display "Mother's side:\n")
 (lst-mb Mb)
 (display "Father's side:\n")
 (lst-pb Pb)
-(display "Combined mother's and father's:\n")
+(display "All members of the tree:\n")
 (lst-all Mb Pb)
+(display "The parents in the given branch:\n")
+(parents Mb)
